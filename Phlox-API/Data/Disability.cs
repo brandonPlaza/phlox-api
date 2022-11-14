ceb722fc71cf7c52501d65808c51dfd2d88c9ae6
brandonPlaza <plazab@sheridancollege.ca>
2022-11-14 14:58:43 -0500

    Created UserDTO class to help recieve and parse user data

---
+++ Phlox-API/Data/Disability.cs
@@ -0,0 +1,15 @@
+﻿namespace Phlox_API.Data
+{
+    public enum TypeOfDisability
+    {
+        Visual,
+        Auditory,
+        Physical
+    }
+    public class Disability
+    {
+        public TypeOfDisability Type { get; set; }
+        public string Description { get; set; }
+
+    }
+}
--- Phlox-API/DatabaseContext/PhloxDbContext.cs
+++ Phlox-API/DatabaseContext/PhloxDbContext.cs
@@ -1,11 +1,26 @@
 ﻿namespace Phlox_API.DatabaseContext
 {
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
+    using Phlox_API.Models;
     using System;
     using System.Collections.Generic;
 
-    public class PhloxDbContext : DbContext
+    public class PhloxDbContext : IdentityDbContext
     {
+        public DbSet<UserDTO> Users { get; set; }
 
+        public string DbPath { get; }
+
+        public PhloxDbContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "phlox.db");
+
+        }
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite($"Data Source={DbPath}");
     }
 }
