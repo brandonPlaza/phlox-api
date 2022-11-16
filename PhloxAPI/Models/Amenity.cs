75a5d1acd3f7b32646e6dbad1494fddd5f511e64
brandonPlaza <plazab@sheridancollege.ca>
2022-11-16 12:41:51 -0500

    Created disability and user classes to facilitate user creation with a list of the users disabilities

--- PhloxAPI/Models/Amenity.cs
+++ PhloxAPI/Models/Amenity.cs
@@ -1,7 +1,16 @@
 ﻿namespace PhloxAPI.Models
 {
+    public enum AmenityType
+    {
+        Elevator,
+        Ramp,
+        AutomaticDoor
+    }
     public class Amenity
     {
         public Guid Id { get; set; }
+        public AmenityType Type { get; set; }
+        public List<Amenity> ConnectedAmenities { get; set; }
+        public bool IsOutOfService { get; set; } = false;
     }
 }
---
+++ PhloxAPI/Models/Disability.cs
@@ -0,0 +1,14 @@
+﻿namespace PhloxAPI.Models
+{
+    public enum DisabilityType
+    {
+        Phisical,
+        Auditory,
+        Visual
+    }
+    public class Disability
+    {
+        public Guid Id { get; set; }
+        public DisabilityType Type { get; set; }
+    }
+}
