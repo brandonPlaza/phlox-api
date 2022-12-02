dbd5bcafb3c262acfdb534da4b27b8e65d016a51
brandonPlaza <plazab@sheridancollege.ca>
2022-12-01 23:16:09 -0500

    Integrated jwt with login function so that a token will be provided for the user to authenticate with the API

--- PhloxAPI/Controllers/AccountsController.cs
+++ PhloxAPI/Controllers/AccountsController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PhloxAPI.DTOs;
+using PhloxAPI.Models;
 using PhloxAPI.Services.AccountsService;
 
 namespace PhloxAPI.Controllers
@@ -13,6 +15,20 @@ namespace PhloxAPI.Controllers
         public AccountsController(IAccountsService accountsService)
         {
             _accountsService = accountsService;
+        }
+
+        [HttpPost("/login")]
+        public IActionResult Login(UserLoginDTO userLogin)
+        {
+            var token = _accountsService.Login(userLogin.Username, userLogin.Password);
+            return Ok(token);
+        }
+
+        [HttpPost("/register")]
+        public IActionResult Register(UserDTO user)
+        {
+            string result = _accountsService.RegisterUser(user.FirstName, user.LastName, user.Username, user.Email, user.Password);
+            return Ok(result);
         }
     }
 }
--- PhloxAPI/Services/AccountsService/AccountsService.cs
+++ PhloxAPI/Services/AccountsService/AccountsService.cs
@@ -1,32 +1,63 @@
 ﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using PhloxAPI.Data;
 using PhloxAPI.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace PhloxAPI.Services.AccountsService
 {
     public class AccountsService : IAccountsService
     {
         private readonly PhloxDbContext _context;
+        private readonly IConfiguration _configuration;
 
-        public AccountsService(PhloxDbContext context)
+        public AccountsService(PhloxDbContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
+            //_builder = builder;
         }
 
         public string Login(string username, string password)
         {
-            var user = _context.Users.SingleOrDefault(u => u.Username == username && u.Password == password);
-            if (user != null)
-                return user.Username;
-            else
-                return string.Empty;
+            var user = _context.Users.SingleOrDefault(u => u.Username == username);
+            if (user != null && MatchPasswords(user.Password, password, Convert.FromBase64String(user.Salt)))
+            {
+                List<Claim> claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, user.Username)
+                };
+
+                var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("Jwt:Key").Value));
+
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+                var token = new JwtSecurityToken(
+                    claims: claims,
+                    expires: DateTime.Now.AddDays(1),
+                    signingCredentials: creds
+                );
+
+                var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+                return jwt;
+            }
+            return "";
         }
 
-        public void RegisterUser(string firstname, string lastname, string username, string email, string password)
+        public string RegisterUser(string firstname, string lastname, string username, string email, string password)
         {
             //var newUser = new User { FirstName = firstname, LastName = lastname, Username = username, Email=email, Password = password, Salt = ""};
+
+            if (_context.Users.FirstOrDefault(u => u.Email == email) != null) 
+            {
+                return "Error: A user with that email already exists";
+            }
             
             byte[] salt = new byte[128 / 8];
             while (true)
@@ -54,6 +85,7 @@ namespace PhloxAPI.Services.AccountsService
 
             _context.Users.Add(newUser);
             _context.SaveChanges();
+            return "User created";
         }
 
         private static string HashPassword(string password, byte[] salt)
