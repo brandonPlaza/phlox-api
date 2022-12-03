560cab43f4c38c0ee2f411b6eb00f8397a4566fe
brandonPlaza <plazab@sheridancollege.ca>
2022-12-02 20:19:39 -0500

    Created HashPassword and MatchPassword private functions in AccountsService to handle salting, hashing and comparing user passwords

--- PhloxAPI/Services/AccountsService/AccountsService.cs
+++ PhloxAPI/Services/AccountsService/AccountsService.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.EntityFrameworkCore;
 using PhloxAPI.Data;
 using PhloxAPI.Models;
+using System.Security.Cryptography;
 
 namespace PhloxAPI.Services.AccountsService
 {
@@ -22,11 +24,64 @@ namespace PhloxAPI.Services.AccountsService
                 return string.Empty;
         }
 
-        public void RegisterUser(string firstname, string lastname, string username, string email, string password)
+        public string RegisterUser(string firstname, string lastname, string username, string email, string password)
         {
-            var newUser = new User { FirstName = firstname, LastName = lastname, Username = username, Email=email, Password = password, Salt = ""};
+            if (_context.Users.FirstOrDefault(u => u.Email == email) != null)
+            {
+                return "Error: A user with that email already exists";
+            }
+
+            byte[] salt = new byte[128 / 8];
+            while (true)
+            {
+                using (var rngCsp = new RNGCryptoServiceProvider())
+                {
+                    rngCsp.GetNonZeroBytes(salt);
+                }
+                if (_context.Users.SingleOrDefault(user => user.Salt == Convert.ToBase64String(salt)) == null)
+                {
+                    break;
+                }
+            }
+
+            var hashedPass = HashPassword(password, salt);
+
+            var newUser = new User
+            {
+                FirstName = firstname,
+                LastName = lastname,
+                Username = username,
+                Email = email,
+                Password = hashedPass,
+                Salt = Convert.ToBase64String(salt)
+            };
+
             _context.Users.Add(newUser);
             _context.SaveChanges();
+            return "User created";
+        }
+
+        private static string HashPassword(string password, byte[] salt)
+        {
+            return Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 100000,
+                numBytesRequested: 256 / 8
+            ));
+        }
+
+        private static bool MatchPasswords(string hashedUserPass, string plaintextUserPass, byte[] salt)
+        {
+            string tempHash = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: plaintextUserPass,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 100000,
+                numBytesRequested: 256 / 8
+            ));
+            return hashedUserPass.Equals(tempHash);
         }
     }
 }
