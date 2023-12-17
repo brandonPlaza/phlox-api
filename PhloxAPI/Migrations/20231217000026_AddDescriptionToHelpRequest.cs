89c6b2598823ac5ead1e963f3b9131c13bc1d731
Angela Villadiego <villadia@sheridancollege.ca>
2023-12-16 19:11:44 -0500

    PMP-488 / PMP-489 PMP-490 API now handles descriptions on help requests

---
+++ PhloxAPI/Migrations/20231217000026_AddDescriptionToHelpRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhloxAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionToHelpRequest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "HelpRequests",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "HelpRequests");
+        }
+    }
+}
